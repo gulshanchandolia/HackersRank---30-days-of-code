4de5ca4b33bafc044baf5d16f2851ee93071e7f2
Gulshan <chandoliagulshan.com>
2021-03-11 17:52:38 +0530

    Day 02

---
+++ Day 02/Day 1-  Data Types.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i1 = int(input())
+d1 = float(input())
+s1 = input()
+# Read and save an integer, double, and String to your variables.
+i = i+i1
+d = d+d1
+s = s+s1
+# Print the sum of both integer variables on a new line.
+print(i)
+print(d)
+print(s)
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
